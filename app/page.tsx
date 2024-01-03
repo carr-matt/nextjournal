85ce803d7b8a5dd855fefabecdfd684d29b5fc24
Matt <carr.matthew.lee@gmail.com>
2024-01-02 23:24:39 -0600

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main>
-      <div>Hi Dad</div>
+    <main className="w-screen h-screen bg-black flex justify-center items-center text-white/90">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-8xl mb-5">A journaling app.</h1>
+        <p className="text-3xl text-white/65 mb-5">
+          Securely journal the day&apos;s events and let AI track your mood.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-purple-900 py-3 px-5 rounded-md text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
