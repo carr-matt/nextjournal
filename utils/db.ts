53758e6c8864d9ace25bd9b8d92101ad43f87ea8
Matt <carr.matthew.lee@gmail.com>
2024-01-04 00:30:31 -0600

    Prevent hot reloading from creating new instances of PrismaClient

---
+++ utils/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+// Prevent hot reloading from creating new instances of PrismaClient
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
