6bd5b9ad8fc2a54e04087ccc3414d7c407283b0e
Matt <carr.matthew.lee@gmail.com>
2024-01-09 01:02:17 -0600

    updated parser, added prompt function, fixed deprecated langchain import statements

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,13 +1,14 @@
-import { OpenAI } from 'langchain/llms/openai';
+import { OpenAI } from '@langchain/openai';
 import { z } from 'zod';
 import { StructuredOutputParser } from 'langchain/output_parsers';
-import { PromptTemplate } from 'langchain/prompts';
+import { PromptTemplate } from '@langchain/core/prompts';
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     mood: z
       .string()
       .describe('the overall mood of the person who wrote the journal entry.'),
+    subject: z.string().describe('the subject of the journal entry.'),
     summary: z.string().describe('brief summary of the entire journal entry.'),
     negative: z
       .boolean()
@@ -20,8 +21,31 @@ const parser = StructuredOutputParser.fromZodSchema(
   })
 );
 
-export const analyze = async (prompt) => {
+const getPrompt = async (content) => {
+  const formattedInstructions = parser.getFormatInstructions();
+
+  const prompt = new PromptTemplate({
+    template:
+      'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{formattedInstructions}\n{entry}',
+    inputVariables: ['entry'],
+    partialVariables: { formattedInstructions },
+  });
+
+  const input = await prompt.format({
+    entry: content,
+  });
+
+  return input;
+};
+
+export const analyze = async (content) => {
+  const input = await getPrompt(content);
   const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
-  const result = await model.invoke(prompt);
-  console.log(result);
+  const result = await model.invoke(input);
+
+  try {
+    return parser.parse(result);
+  } catch (e) {
+    console.log(e);
+  }
 };
