0a8b41906785acd45910553aa4f89f8707e1ba4e
Matt <carr.matthew.lee@gmail.com>
2024-01-17 01:01:27 -0600

    updated description

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default async function Home() {
   return (
     <main className="w-screen h-screen bg-black flex justify-center items-center text-white/90">
       <div className="w-full max-w-[600px] mx-auto">
-        <h1 className="text-8xl mb-5">A journaling app.</h1>
+        <h1 className="text-8xl mb-5">A journal app.</h1>
         <p className="text-3xl text-white/65 mb-5">
-          Securely journal the day&apos;s events and let AI track your mood.
+          Chronicle each day. Let AI track your mood.
         </p>
         <div>
           <Link href={href}>
