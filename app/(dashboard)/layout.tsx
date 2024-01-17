84804df75e282e2769bd70b35e92633537421f5c
Matt <carr.matthew.lee@gmail.com>
2024-01-16 20:39:00 -0600

    nav links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,26 @@
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'Journal', href: '/journal' },
+];
 
 const DashboardLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
-        journal
+        <div>Journal</div>
+        <ul>
+          {links.map((link) => (
+            <li
+              key={link.href}
+              className="px-2 py-6 text-xl"
+            >
+              <Link href={link.href}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[80px] border-b border-black/10">
