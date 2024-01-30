7d40d7a451f3e612920df348caf674dd688ac90f
Matt <carr.matthew.lee@gmail.com>
2024-01-30 16:53:48 -0600

    centered clerk components

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,9 +2,13 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <SignUp
-      afterSignUpUrl="/new-user"
-      redirectUrl="/new-user"
-    />
+    <div className="w-screen h-screen grid grid-cols-1 place-content-center">
+      <div className="self-center mx-auto">
+        <SignUp
+          afterSignUpUrl="/new-user"
+          redirectUrl="/new-user"
+        />
+      </div>
+    </div>
   );
 }
